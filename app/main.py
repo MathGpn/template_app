cd4e2e3677ff7112eef5a92e67af33ec68af811e
MathGpn <grosperrin@eisti.eu>
2023-10-06 16:03:14 +0200

    Modifications

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ st.set_page_config(layout="wide",page_title='MathGpn App',page_icon=img)
 st.header("Mon application conteneurisée et poussée sur Azure")
 
 st.sidebar.warning("Authentification")
-st.success(os.getlogin())
+# st.success(os.getlogin())
 #st.success(getpass.getuser())
 #pwd.getpwuid(os.getuid())[0]
 #print("Voici votre identifiant :", os.getlogin())
